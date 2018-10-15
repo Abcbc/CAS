33961baa623e41c03b96d28c20ef9dd70ef39537
Lasse Lueder <lasse.lueder@lueder-mail.de>
2018-10-15 07:53:50 +0200

    calculate V ('anerk. Vermoegen')

--- Rules.py
+++ Rules.py
@@ -4,6 +4,7 @@ import networkx as nx
 KEY_SPECTRUM = 'spectrum'
 KEY_OPINIONS = 'opinions'
 KEY_ORIENTATION = 'orientation'
+KEY_V = 'V'
 
 def calcSpectrum(opinions):
     spectrum = 0
@@ -20,18 +21,34 @@ def calcOrientation(nodeA, nodeB):
     
     return orientation
 
+def calcV(edges): # "anerkanntes Vermoegen"
+    V = 0
+    for edge in edges:
+        V += edge[KEY_ORIENTATION] * edge[KEY_ORIENTATION]
+    
+    return V
+
+def getEdgesFromIndices(graph, ids):
+    edges = []
+    for id in ids:
+        edges.append(graph.edges[id])
+    return edges
+
 class AttributesUpdateRule:
 #     graph = nx.Graph()
 #     selectionRule = selectNodeFromGraph(graph)
     def apply(self, graph): # TODO: not the rule should have the apply method?
-        for nodeId in graph.nodes:
-            node = graph.nodes[nodeId]
-            node[KEY_SPECTRUM] = calcSpectrum(node[KEY_OPINIONS])
-            
         for edgeId in graph.edges:
             edge = graph.edges[edgeId]
             neighborA = graph.nodes[edgeId[0]]
             neighborB = graph.nodes[edgeId[1]]
             edge[KEY_ORIENTATION] = calcOrientation(neighborA, neighborB)
+            
+        for nodeId in graph.nodes:
+            node = graph.nodes[nodeId]
+            node[KEY_SPECTRUM] = calcSpectrum(node[KEY_OPINIONS])
+            
+            edges = getEdgesFromIndices(graph, graph.edges(nodeId))
+            node[KEY_V] = calcV(edges)
             
         return graph
