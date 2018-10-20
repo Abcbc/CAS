efd9597da07bdad0f5913ac81fe85dcebe666f07
Lasse Lueder <lasse.lueder@lueder-mail.de>
2018-10-20 12:09:04 +0200

    Base Ruĺes on base class

--- Rules.py
+++ Rules.py
@@ -2,7 +2,47 @@ import random
 from SelectionRules import selectEdgeFromGraph, selectOpinionPairFromGraph
 from Graph import KEY_OPINIONS, doOpinionsDiffer, areOppositeOpinions
 
-class OrientationConfirmationRule:
+class Rule:
+    '''
+    Returns the human-readable and unique name for this rule
+    '''
+    @staticmethod
+    def getName():
+        raise NotImplementedError('getName not implemented for this rule class')
+
+    '''
+    Returns a dictionary with internal parameters which are used in this rule
+    '''
+    def getParameters(self):
+        raise NotImplementedError('getParameters not implemented for this rule class')
+
+    '''
+    Sets internal parameters from the dictionary (can be retrieved with getParameters)
+    '''
+    def setParameters(self, parameters):
+        raise NotImplementedError('setParameters not implemented for this rule class')
+
+    '''
+    Returns the operands to be passed to apply()
+    '''
+    def getOperands(self, graph):
+        raise NotImplementedError('getOperands not implemented for this rule class')
+
+    '''
+    Applies this rule to the operands of the graph.
+    Operands have to be parts of the graph and have the structure as returned by getOperands().
+    Returns the graph, which can be a new object.
+    '''
+    def apply(self, graph, operands):
+        raise NotImplementedError('apply not implemented for this rule class')
+
+'''
+Parameters: None
+'''
+class OrientationConfirmationRule(Rule):
+    def getParameters(self):
+        return dict()
+
     def getOperands(self, graph):
         print('Selecting operands for OrientationConfirmationRule')
         return selectEdgeFromGraph(graph)
@@ -33,8 +73,14 @@ class OrientationConfirmationRule:
     @staticmethod
     def getName():
         return 'OrientationConfirmationRule'
-                    
-class AdaptationRule:
+
+'''
+Parameters: None
+'''
+class AdaptationRule(Rule):
+    def getParameters(self):
+        return dict()
+
     def getOperands(self, graph):
         print('Selecting operands for AdaptationRule')
         # ToDo always chooses the same edge with the weight_getter_edge lambda, why?
