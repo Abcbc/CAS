a5dbe6fb3817c86e1355854a39d8c4180d496bf3
Lasse Lueder <lasse.lueder@lueder-mail.de>
2018-11-15 21:53:18 +0100

    Builder use given graph as base

--- Builder.py
+++ Builder.py
@@ -5,8 +5,10 @@ from Graph import addConvenienceAttributes
 def getRandomOpinion():
     return random.randint(-1,1)
 
-def buildGraph():
-    g = nx.complete_graph(3)
+# use g as a base if given
+def buildGraph(g=None):
+    if g is None:
+        g = nx.complete_graph(3)
     for nodeId in g.node:
         g.nodes()[nodeId]['opinions']=[getRandomOpinion(),getRandomOpinion(),getRandomOpinion()]
         g.nodes()[nodeId]['spectrum']=0
