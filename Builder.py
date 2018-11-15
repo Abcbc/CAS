2f97abb99c8f26da7d505523b19aa37bae6782cb
Lasse Lueder <lasse.lueder@lueder-mail.de>
2018-11-15 21:53:36 +0100

    Builder add method to build test graph for NewEdgesRule

--- Builder.py
+++ Builder.py
@@ -18,3 +18,29 @@ def buildGraph(g=None):
 
     addConvenienceAttributes(g)
     return g
+
+def buildTestGraphForNewEdgeRule(degreeToSearch=1):
+    g = nx.Graph()
+
+    if degreeToSearch == 0: # special case: all connected
+        g = nx.complete_graph(3)
+    elif degreeToSearch == 1:
+        g.add_edge(0,1)
+        g.add_edge(1,2)
+        g.add_edge(1,3)
+    elif degreeToSearch == 2:
+        g.add_edge(0,1)
+        g.add_edge(0,2)
+        g.add_edge(1,2)
+        g.add_edge(2,3)
+    elif degreeToSearch == 3:
+        g.add_edge(0,1)
+        g.add_edge(0,2)
+        g.add_edge(0,3)
+        g.add_edge(1,2)
+        g.add_edge(2,3)
+        g.add_edge(3,4)
+    else:
+        raise NotImplementedError('degreeToSearch to high (between 0 and 3)')
+
+    return buildGraph(g)
