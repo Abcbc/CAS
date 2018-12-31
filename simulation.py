b0d666bd6dc3a03ae9ee45b8ce073021754b3005
Lasse Lueder <lasse.lueder@lueder-mail.de>
2018-12-31 11:38:38 +0100

    parallized simulations

--- simulation.py
+++ simulation.py
@@ -1,6 +1,7 @@
 import utils.ConfigLoader as cnf
 from GraphFactory import GraphFactory
 import networkx as nx
+import multiprocessing as mp
 
 from utils.Logger import *
 log = get_logger(__name__, __file__) # For Main, call before any include with also calls get_logger
@@ -9,8 +10,17 @@ import Updater
 import GraphLog as gl
 import Rules
 
+def _run(ruleset, graph, logDir, repetition, iterations):
+    updater = Updater.Updater(ruleset)
+    updater.setGraph(graph, get_graph_logger('GraphLogger_'+logDir+'graph_'+str(repetition), logDir+'graph_'+str(repetition)+'.log'))
 
-def run_simulation(simulation_setting, logDir):
+    for iteration in range(iterations):
+        updater.update()
+
+    updater.close()
+
+
+def run_simulation(simulation_setting, logDir, pool):
     log.debug(simulation_setting)
     gf = GraphFactory(simulation_setting)
 
@@ -21,19 +31,16 @@ def run_simulation(simulation_setting, logDir):
         for rulename, rule in ruleset.items():
             rule.setParameters(simulation_setting[rulename])
 
-        updater = Updater.Updater(ruleset)
-        updater.setGraph(g, get_graph_logger('GraphLogger', logDir+'graph_'+str(repetition)+'.log'))
+        pool.apply_async(_run, args=(ruleset, g, logDir, repetition, simulation_setting["sim_iterations"]))
 
-        for iteration in range(simulation_setting["sim_iterations"]):
-            updater.update()
-
-    updater.close()
 
     #gExe = g.GraphLogExecuter(gl.GraphLogReader('logs/graph.log'))
     #gExe.performSteps(20)
 
 
 def main():
+    pool = mp.Pool()
+
     log.info("Loading Config.")
     settings = cnf.load_config()
     for simulation_setting in settings:
@@ -52,7 +59,10 @@ def main():
             except(FileExistsError):
                 pass
             cnf.save_config(stepConfig,  stepDir+'settings.yaml')
-            run_simulation(stepConfig, stepDir)
+            run_simulation(stepConfig, stepDir, pool)
+
+    pool.close()
+    pool.join()
 
 if __name__ == "__main__":
     main()
