b49842b6d7ba21dab896c8f62d45a1ea4b943c04
Lasse Lueder <lasse.lueder@lueder-mail.de>
2019-01-15 18:04:30 +0100

    Updater weight choose rule with weight

--- Updater.py
+++ Updater.py
@@ -8,12 +8,18 @@ import Analyser
 class Updater:
     default_config = {
         'logging_enabled': False,
+        'OrientationConfirmationRule_weight': 1,
+        'AdaptationRule_weight': 1,
+        'NewNodeRule_weight': 1,
+        'NewEdgesRule_weight': 1,
+        'RemoveEdgeRule_weight': 1,
+        'TakeoverRule_weight': 1,
     }
 
     def __init__(self, ruleset=None, config=default_config):
         if ruleset is None:
             ruleset = getRuleset()
-        self.rules = ruleset
+        self.rules = {rulename:{'rule':rule,'weight':config[rulename+'_weight']} for rulename, rule in ruleset.items()}
         self.analyzer = Analyser.Analyser()
         self.writeLog = config['logging_enabled']
 
@@ -28,27 +34,24 @@ class Updater:
         self.analyzer.initAnalysis(self.graph, config={'stepSize':10})
         self.analyzer.onNewVersion(self.graph)
 
-    def addRule(self, rule):
-        self.rules[rule.getName()] = rule
-
     def update(self, ruleName = None):
         self.applyRule(ruleName)
 
     def applyRule(self, ruleName=None):
         if ruleName is None:
-            ruleName = random.choice(list(self.rules))
+            ruleName = random.choices(list(self.rules), [rule['weight'] for rule in self.rules.values()],k=1)[0]
 
         # TODO: get rule's requirements and choose items
         # for the rule to operate on
         try:
-            self.graph = self.rules[ruleName].apply(self.graph)
+            self.graph = self.rules[ruleName]['rule'].apply(self.graph)
 
             self.graph = Graph.calculateAttributes(self.graph)
 
             Graph.incrementVersion(self.graph)
 
             if self.writeLog:
-                self.graphLogger.logRule(self.rules[ruleName])
+                self.graphLogger.logRule(self.rules[ruleName]['rule'])
 
             self.analyzer.onNewVersion(self.graph)
         except TimeoutError:
